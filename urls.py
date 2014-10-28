9b7ccda51eef25d5ebbe3e3dbda299837519f11a
geosav <geo.savchuk@gmail.com>
2014-10-28 10:26:59 +0200

    added adminpanel: login template

--- urls.py
+++ urls.py
@@ -2,20 +2,17 @@ from django.conf.urls import patterns, include, url
 import settings
 
 from django.contrib import admin
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r"^$", "apps.home.views.index", name="home"),
-                       url(r"^accounts/", include("django.contrib.auth.urls")),
-                       url(r"^rules/", include("apps.rules.urls", namespace="rules")),
-                       url(r"^advertisement/", include("apps.advertisement.urls", namespace="advertisement")),
-                       url(r"^faq/", include("apps.faq.urls", namespace="faq")),
-                       url(r"^contacts/", include("apps.contacts.urls", namespace="contacts")),
-                       # url(r'^blog/', include('blog.urls')),
+    url(r"^$", "apps.home.views.index", name="home"),
+    url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^rules/", include("apps.rules.urls", namespace="rules")),
+    url(r"^advertisement/", include("apps.advertisement.urls", namespace="advertisement")),
+    url(r"^faq/", include("apps.faq.urls", namespace="faq")),
+    url(r"^contacts/", include("apps.contacts.urls", namespace="contacts")),
 
-                       #url(r'^adminpanel/', include(adminpanel.site.urls)),
-                       url(r"^site_admin/", include("apps.adminpanel.urls", namespace="adminpanel")),
-                       (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-                           'document_root': settings.MEDIA_ROOT})
+    url(r"^adminpanel/", include("apps.adminpanel.urls", namespace="adminpanel")),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT})
 )
