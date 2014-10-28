25ce06cf940145996b6e8bbca4adab9b4d018cd6
geosav <geo.savchuk@gmail.com>
2014-10-28 22:59:54 +0200

    added model and admin to internal

--- apps/internal/models.py
+++ apps/internal/models.py
@@ -2,27 +2,109 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-PROJECT_TYPE_CHOICES = (
-    ("web_project", _("Web Project")),
-    ("stand_alone_application", _("Stand Alone Application")),
-    ("cloud_application", _("Cloud Application"))
-)
+class Region(models.Model):
+
+    name = models.CharField(_("Область"), blank=False, max_length=20)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class City(models.Model):
+
+    name = models.CharField(_("Місто"), blank=False, max_length=30)
+    region = models.ForeignKey(Region,  verbose_name=_("Область"))
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class Hospital(models.Model):
+
+    name = models.CharField(_("Назва клініки"), blank=False, max_length=75)
+    is_state = models.BooleanField(_("Державна"), default=True)
+    type = models.CharField(_("Тип клініки"), blank=False, max_length=25)
+    city = models.ForeignKey(City, verbose_name=_("Місто"))
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class Speciality(models.Model):
+
+    name = models.CharField(_("Спеціальність"), blank=False, max_length=30)
+
+    def __unicode__(self):
+        return unicode(self.name)
 
 
 class Doctor(models.Model):
 
-    first_name = models.CharField(_("First Name"), blank=False, max_length=20)
-    last_name = models.CharField(_("Last Name"), blank=False, max_length=20)
-    email = models.EmailField(_("Email"), blank=False, max_length=50)
-    phone = models.CharField(_("Phone"), blank=True, max_length=20)
-    skype = models.CharField(_("Skype"), blank=True, max_length=30)
-    summary = models.TextField(
-        _("Developer's Summary Profile Info"), max_length=700, blank=True)
-    linkedin_profile = models.URLField(_("LinkedIn URL"), blank=True)
-    github_username = models.CharField(_("GitHub User Name"), blank=True,
-                                       max_length=20)
-    resume_file = models.FileField(_("Resume"), upload_to="developer_resume/",
-                                   null=True, blank=True)
+    first_name = models.CharField(_("Прізвище"), blank=False, max_length=30)
+    last_name = models.CharField(_("Ім'я"), blank=False, max_length=25)
+    patronymic = models.CharField("По батькові", blank=False, max_length=30)
+    speciality = models.ForeignKey(Speciality, verbose_name=_("Спеціальність"))
+    hospitals = models.ManyToManyField(Hospital, blank=False, verbose_name=_("Клініка"))
+    image = models.ImageField(_("Зображення"), upload_to="doctor_photos/",
+                              null=True, blank=True)
 
     def __unicode__(self):
         return u"%s %s" % (self.first_name, self.last_name)
+
+
+class Efficiency(models.Model):
+
+    rating = models.IntegerField("Рейтинг", blank=True)
+    doctor = models.ForeignKey(Doctor, verbose_name=_("Лікар"))
+
+    def __unicode__(self):
+        return unicode(self.doctor)
+
+
+class Quality(models.Model):
+
+    rating = models.IntegerField("Рейтинг", blank=True)
+    doctor = models.ForeignKey(Doctor, verbose_name=_("Лікар"))
+
+    def __unicode__(self):
+        return unicode(self.doctor)
+
+
+class Respect(models.Model):
+
+    rating = models.IntegerField("Рейтинг", blank=True)
+    doctor = models.ForeignKey(Doctor, verbose_name=_("Лікар"))
+
+    def __unicode__(self):
+        return unicode(self.doctor)
+
+
+class Bribery(models.Model):
+    rating = models.IntegerField("Рейтинг", blank=True)
+    doctor = models.ForeignKey(Doctor, verbose_name=_("Лікар"))
+
+    def __unicode__(self):
+        return unicode(self.doctor)
+
+
+class Comment(models.Model):
+
+    content = models.TextField(_("Коментар"), blank=False, max_length=1000)
+    plus = models.TextField(_("Позитивні якості"), blank=True, max_length=300)
+    minus = models.TextField(_("Негативні якості"), blank=True, max_length=300)
+    created = models.DateTimeField(_("Дата створення"), auto_now=False, auto_now_add=True)
+    edited = models.DateTimeField(_("Дата редагування"), auto_now=True, auto_now_add=False)
+    doctor = models.ForeignKey(Doctor, verbose_name=_("Лікар"))
+
+    def __unicode__(self):
+        return unicode(self.doctor)
+
+
+class Like(models.Model):
+
+    rating = models.IntegerField("Рейтинг", blank=True)
+    comment = models.ForeignKey(Comment, verbose_name=_("Коментар"))
+    ip = models.IPAddressField()("IP-адрес")
+
+    def __unicode__(self):
+        return unicode(self.сomment)
