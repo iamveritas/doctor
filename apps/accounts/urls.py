1e03e11755422bc26b5666e2fafd7aece7c43348
geosav <geo.savchuk@gmail.com>
2015-02-18 15:42:37 +0000

    added simple forgot password

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 import settings
+from django.contrib.auth import views
 from apps.accounts.views import (UserLogin, UserDetail, UserDoctorCreate, UserCreate,
                                  UserReviewList, logout, user_settings, user_settings_doctor)
 #from django.contrib.auth.decorators import login_required
@@ -15,4 +16,16 @@ urlpatterns = patterns("",
     url(r'^logout/$',logout, name="logout"),
     url(r'^settings/$',user_settings, name="user_settings"),
     url(r'^settingsdoc/$',user_settings_doctor, name="user_settings_doctor"),
+
+     url(r'^password/reset/$',
+        'django.contrib.auth.views.password_reset',
+        {'post_reset_redirect' : '/accounts/password/reset/done/'},
+        name="password_reset"),
+    (r'^password/reset/done/$',
+        'django.contrib.auth.views.password_reset_done'),
+    (r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {'post_reset_redirect' : 'accounts/password/done/'}),
+    (r'^password/done/$',
+        'django.contrib.auth.views.password_reset_complete'),
 )
